8737ff507aaace6ed72cb929f399d20c035e5297
Choudhary Himanshu <himanshuch3003@gmail.com>
2025-11-19 18:33:35 +0530

    feat: implement video playback functionality with escape key support

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Banner } from "@/components/Banner";
 import { useAllVideos } from "@/api/hooks/getVideos";
 import { FaPlay } from "react-icons/fa6";
@@ -7,6 +7,22 @@ import { FaPlay } from "react-icons/fa6";
 export default function Home() {
   const { data, isLoading, isError } = useAllVideos();
   const [isOpen, setIsOpen] = useState(false);
+  const [videoUrl, setVideoUrl] = useState("");
+
+  useEffect(() => {
+  const handleEsc = (e:KeyboardEvent) => {
+    if (e.key === "Escape") {
+      handleVideoPlay({ open: false, url: "" });
+    }
+  };
+
+  window.addEventListener("keydown", handleEsc);
+  return () => window.removeEventListener("keydown", handleEsc);
+}, []);
+  const handleVideoPlay=({ open, url }: { open: boolean; url: string }) => {
+    setVideoUrl(url);
+    setIsOpen(open);
+  };
   return (
     <div className="font-sans items-center min-h-screen p-4 space-y-6">
       <Banner />
@@ -22,7 +38,7 @@ export default function Home() {
                 className="rounded-lg w-[25rem] aspect-video bg-black"
               />
               <button
-                onClick={() => setIsOpen(true)}
+                onClick={() => handleVideoPlay({open:true, url:video.url})}
                 className="relative -top-[8rem] right-[-40%] w-fit cursor-pointer"
               >
                 <FaPlay className="text-red-500 text-6xl opacity-75 hover:opacity-100" />
@@ -31,6 +47,19 @@ export default function Home() {
                 {video.key}
               </span>
             </div>
+            {isOpen && (
+              <div
+                className="fixed inset-0 bg-white w-[99%] h-screen p-4 bg-opacity-75 flex items-center justify-center z-50 rounded-lg mx-auto"
+                onClick={() => handleVideoPlay({open:false, url:""})}
+              >
+                <video
+                  src={videoUrl}
+                  className="rounded-lg w-[80%] h-[80%] bg-black"
+                  controls
+                  autoPlay
+                />
+              </div>
+            )}
           </div>
         ))}
       </div>
